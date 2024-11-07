9e5bdd7c03efa745e37d0ad45d27d1a51666214e
yskoht <ysk.oht@gmail.com>
2024-11-07 21:41:29 +0900

    Update version 0.5 -> 0.6

--- github-show-filename-list.user.js
+++ github-show-filename-list.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Show Filename List
 // @namespace    https://github.com/yskoht/github-show-filename-list
-// @version      0.5
+// @version      0.6
 // @description  Show filename list on "Files changed" tab in GitHub
 // @author       yskoht
 // @match        https://github.com/*
